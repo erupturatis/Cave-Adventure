cf5cff23dd9e5f80cb1ca81cfc7ea1b85fe30efa
erupturatis <bb.octav@gmail.com>
2021-10-09 18:13:24 +0300

    finished air rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,58 +1,126 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Movement : MonoBehaviour
 {
+    bool IsOnGround = false;
+
     Rigidbody2D rb;
+
     public Transform Down;
     public Transform Forward;
     public Transform Up;
-    public float force = 6f;
-    public float ForceJump = 5f;
-    bool IsOnGround = false;
 
-    private Vector2 NewVelocity;
+    [SerializeField]
+    private float force;
+    [SerializeField]
+    private float ForceJump;
+    [SerializeField]
+    private float rotationSpeed;
+    [SerializeField]
+    private float slopeCheckDistance;
+    private float slopeDownAngle;
+
+    private Vector2 newVelocity;
+    private Vector2 slopeNormalPerp;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    BoxCollider2D bx;
+    
+    Vector2 colliderSize;
+
+    [SerializeField]
+    private PhysicsMaterial2D noFriction;
+    [SerializeField]
+    private PhysicsMaterial2D fullFriction;
+
+    Quaternion Q;
+
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
+        bx = GetComponent<BoxCollider2D>();
+        colliderSize = bx.size;
+        rb.sharedMaterial = noFriction;
     }
 
-    
-    void Update()
+    private void SlopeCheck()
     {
+        Vector2 checkPos = transform.position;
+        SlopeCheckVertical(checkPos);
 
-        //print(force);
-        if (Input.GetKeyDown("w") && IsOnGround)
+    }
+
+    private void SlopeCheckVertical(Vector2 checkPos)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance,whatIsGround);
+        if (hit)
         {
-            print("jumpuit");
-            NewVelocity.Set(0f,0f);
-            //rb.velocity = NewVelocity;
-            Vector2 NewForce = new Vector2( transform.position.x - Up.position.x , transform.position.y - Up.position.y);
-            rb.AddForce(NewForce * -ForceJump , ForceMode2D.Impulse);
+            slopeNormalPerp = Vector2.Perpendicular(hit.normal).normalized;
+            Q = Quaternion.Euler(Quaternion.FromToRotation(Vector3.up, hit.normal).eulerAngles); // calculating the angle of the slope
+
+            slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            Debug.DrawRay(hit.point, slopeNormalPerp, Color.red);
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
+        }
+
+    }
+
+
+    private void ApplyMovement()
+    {
+        SlopeCheck();
+        if (Input.GetKey("w") && IsOnGround)
+        {
+            Vector2 NewForce = new Vector2(transform.position.x - Up.position.x, transform.position.y - Up.position.y);
+            rb.AddForce(NewForce * -ForceJump, ForceMode2D.Impulse);
             IsOnGround = false;
         }
-        if (IsOnGround)
-        {
-            NewVelocity.Set(Forward.position.x - gameObject.transform.position.x, Forward.position.y - gameObject.transform.position.y);
-            rb.velocity = NewVelocity * force;
-        }
         
         if (IsOnGround)
         {
-            //print("a inceput");
-            rb.gravityScale = 0;
-            Vector3 direction = Down.position - transform.position;
-            rb.AddForce(direction * force );
-            
-        }
-        else
-        {
-            rb.gravityScale = 1;
 
+            gameObject.transform.rotation = Q; // gameobject has the same angle as the slope
+            newVelocity.Set(Forward.position.x - gameObject.transform.position.x, Forward.position.y - gameObject.transform.position.y);
+            rb.velocity = newVelocity * force;
+            Vector3 V = new Vector3(Down.transform.position.x - transform.position.x, Down.transform.position.y - transform.position.y, 0f);
+            rb.AddForce(V * force * 3); // add force down to keep player on track
         }
-        //print(IsOnGround);
+
+    }
+
+    private void RotateMidAir()
+    {
+        if (IsOnGround)
+        {
+            return;
+        }
+        //Debug.Log(gameObject.transform.rotation.eulerAngles.z + " " + Q.eulerAngles.z  + "   " + rb.rotation);
+        float direction = (transform.rotation.eulerAngles.z-Q.eulerAngles.z);
+        if (Mathf.Abs(direction) > 180f)
+        {
+            direction = -direction;
+        }
+        direction = -direction;
+        rb.MoveRotation(transform.rotation.eulerAngles.z + Mathf.Sign(direction) * rotationSpeed*Time.fixedDeltaTime);
+
+    }
+
+
+    void Update()
+    {
         
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+        RotateMidAir();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
